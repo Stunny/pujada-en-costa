2939a6e39b9953d885d67987abda7719a36b1e8f
Alex Vogel <avogelm@gmail.com>
2016-11-26 18:34:53 +0100

    Añadido main

---
+++ SOFT_BASE/SoftMain.c
@@ -0,0 +1,42 @@
+//
+//-------------------------PROGRAMA DE TEST DE l'LSMAKER--------------------
+//
+// Aquest és el programa que porta carregat "de fàbrica" l'LSMaker.
+// Només fa que moure els motors endavant i endarrera i enviar una
+// trama per l'enllaç de radiofreqüencia. Per la pantalla de l'LCD
+// mostra la tensió de la bateria, l'adreça mac, el nom d'usuari 
+// que hagis configurat amb el programa LSLoader, la inclinació XYZ i
+// el valor dels senyals STOP0, STOP1 i STOP2 (Blanc o Negre)
+//
+// Per crear la teva propia aplicació, només cal que baixis el directori
+// LS_API i el directori Proves i ja pots engegar el MPLAB i obrir el projecte
+// TestMain. El projecte ja te tots els tads i l'API ben configurada, només
+// cal que afegeixis els teus propis TADS i la teva aplicació en lloc de 
+// la "ProvaDeCalibracio"
+// 
+
+//------------------------------LLIBRERIES-------------------------
+#include "../LS_API/LSApi.h"
+//-------------------------END--LLIBRERIES-------------------------
+
+//------------------------------CONSTANTS--------------------------
+// Opcions de configuració del microprocessador PIC16F64
+_CONFIG2(OSCIOFNC_ON);
+_CONFIG1(JTAGEN_OFF & ICS_PGx1);
+//-------------------------END--CONSTANTS--------------------------
+
+//------------------------------TIPUS------------------------------
+//-------------------------END--TIPUS------------------------------
+
+void inicialitza() {
+    LS_Init();
+}
+
+int main(void) {
+    
+    inicialitza();
+    while (1) {
+        LS_Executiu();
+    }
+    return 0;
+}
