1565a77a3b0e76be036559e980cdf75fb3892fe4
Alex Vogel <avogelm@gmail.com>
2016-12-14 15:28:49 +0100

    Reestructuracio del software de pujada. El bucle que endereza el robot no actualizaba los valores de las componentes del acelerometro por lo que no podia hacer bien la correccion de rumbo. Mejoradas las funciones de giro sobre si mismo y estructurada la funcion de subida.

--- SOFT_BASE/SoftMain.c
+++ SOFT_BASE/SoftMain.c
@@ -1,21 +1,4 @@
 //
-//----------------------------------------PROGRAMA DE TEST DE l'LSMAKER-----
-//
-// Aquest ?s el programa que porta carregat "de f?brica" l'LSMaker.
-// Nom?s fa que moure els motors endavant i endarrera i enviar una
-// trama per l'enlla? de radiofreq?encia. Per la pantalla de l'LCD
-// mostra la tensi? de la bateria, l'adre?a mac, el nom d'usuari 
-// que hagis configurat amb el programa LSLoader, la inclinaci? XYZ i
-// el valor dels senyals STOP0, STOP1 i STOP2 (Blanc o Negre)
-//
-// Per crear la teva propia aplicaci?, nom?s cal que baixis el directori
-// LS_API i el directori Proves i ja pots engegar el MPLAB i obrir el projecte
-// TestMain. El projecte ja te tots els tads i l'API ben configurada, nom?s
-// cal que afegeixis els teus propis TADS i la teva aplicaci? en lloc de 
-// la "ProvaDeCalibracio"
-// 
-
-//
 //----------------------------------------PROTOTIPUS---------------
 //
 
@@ -72,7 +55,7 @@ void MotorInfo(void) {
 				//LS_LCD_Printf(0,3, "B=%03d", LS_SYS_GetBatteryVoltage());
 				// Els angles XYZ de l'acceler?metre
 				AcGetXYZ(&Xf, &Yf, &Zf); X = Xf; Y = Yf; Z = Zf;
-				LS_LCD_Printf(0,2, "XYZ= %+03d,%+03d,%+03d", X, Y, Z);
+				LS_LCD_Printf(3,2, "XYZ= %+03d,%+03d,%+03d", X, Y, Z);
 				// El valor dels senyals de Stop0, Stop1 i Stop2
 				//LS_LCD_Printf(6,3, "%c%c%c", GetS(MT_STOP_1),GetS(MT_STOP_0), GetS(MT_STOP_2));
 				// El robot est? adquirint?
@@ -81,7 +64,6 @@ void MotorInfo(void) {
 	}
 }
 
-// Subtrutines de moviment
 void AV50(void) { int Stop; int temps;
 	temps = LS_MT_GetTimeFromDistance(50, 60); // 50 cm a velocitat 60
 	LS_MT_Lineal(temps, 60, 0, &Stop);
@@ -92,105 +74,136 @@ void RE50(void) { int Stop; int temps;
 	LS_MT_Lineal(temps, -60, 0, &Stop);
 }
 
+//---------------------------------------END SOFTWARE CALIBRACIO----------------
 
-void GD90(void) { int Stop; int angle;
-	angle = LS_MT_GetTimeFromAngle(90, 40); // 90 graus a 40
-	LS_MT_TurnRight(angle, 40, 0, 0, &Stop);
+
+
+
+//------------PUJADA EN COSTA-------------------------------------------------//
+
+/**
+ * Gir a la dreta del robot
+ * @param angle Angle de gir desitjat
+ * @param speed Velocitat de gir desitjada
+ * @param radius Radi de gir desitjat. 0 per girar sobre si mateix
+ */
+void GD(int angle, int speed, int radius) { 
+    int Stop;
+	int angleGir = LS_MT_GetTimeFromAngle(angle, speed); // Girar a dreta $('angle') graus 
+	LS_MT_TurnRight(angleGir, speed, radius, 0, &Stop);       // a $('speed') 
 }
 
-void GE90(void) { int Stop; int angle;
-	angle = LS_MT_GetTimeFromAngle(90, 40); // 90 graus a 40
-	LS_MT_TurnLeft(angle, 40, 0, 0, &Stop);
+/**
+ * Gir a l'esquerra del robot
+ * @param angle Angle de gir dessitjat
+ * @param speed Velocitat de gir desitjada
+ * @param radius Radi de gir desitjat. 0 per girar sobre si mateix
+ */
+void GE(int angle, int speed, int radius) { 
+    int Stop; 
+	int angleGir = LS_MT_GetTimeFromAngle(angle, speed); // Girar Esquerra $('angle') graus 
+	LS_MT_TurnLeft(angleGir, speed, radius, 0, &Stop);        // a $('speed')
 }
 
-int ProvaDeCalibracio(int enderezado){ 
-	
-    //AV50();
-    int X, Y, Z; float Xf, Yf, Zf, stopReason;
+/**
+ * Procedimient que implementa el comportamient de pujasda del robot
+ * @param X Component X del acelerometre
+ * @param Y Component Y del acelerometre
+ * @param Z Component Z del acelerometre
+ */
+void upYouGo(int X, int Y, int Z){
+    
+    int stopReason;
+    
+     //Subir recto
+    if(Y > -5 && Y < 5){
+        LS_MT_Lineal(LS_MT_GetTimeFromDistance(10, 100), 100, 0, &stopReason);
+        LS_LCD_Printf(1,2, "Subir");
+        return;
+    }
+        
+    //Subir girando a derecha
+    if(Y > -10 && Y < -5){
+        LS_MT_TurnRight(LS_MT_GetTimeFromDistance(10, 100), 100, 7, 0, &stopReason);
+        LS_LCD_Printf(1,2, "Subir Der");
+        return;
+    }
+        
+    //Subir girando a izquierda
+    if(Y < 10 && Y > 5){
+        LS_MT_TurnLeft(LS_MT_GetTimeFromDistance(10, 100), 100, 7, 0, &stopReason);
+        LS_LCD_Printf(1,2, "Subir Izq");
+        return;
+    }
+    
+    if(Y > 10){
+        LS_MT_TurnLeft(LS_MT_GetTimeFromDistance(10, 100), 100, 10, 0, &stopReason);
+        LS_LCD_Printf(1,2, "Subir Izq");
+        return;
+    }
+    
+    if(Y < -10){
+        LS_MT_TurnRight(LS_MT_GetTimeFromDistance(10, 100), 100, 10, 0, &stopReason);
+        LS_LCD_Printf(1,2, "Subir Der");
+        return;
+    }
+}
+
+/**
+ * Procediment principal de la funcionalitat de pujada en costa.
+ * Aquesta funció esta pensada per funcionar dins del bucle infinit 
+ * establert al main(void).
+ */
+void Main(){ 
+
+    int X, Y, Z; float Xf, Yf, Zf;
+    
     AcGetXYZ(&Xf, &Yf, &Zf); X = Xf; Y = Yf; Z = Zf;
     
-    //Enderezar robot hasta estar cuesta arriba (hasta que y = 0 & x < 10)
-    while(!(X < 10) && enderezado == 0){
-        if(Y > 0){
-            //Enderezar derecha
-            LS_MT_TurnRight(LS_MT_GetTimeFromDistance(5, 100), 100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "E Derecha");
+    //Cerca de la posició del robot que el deixi direccio amunt.
+    while(X >= 10){
+        if(Y > 5){
+            //Cerca dreta
+            GE(10, 100, 0);
+            LS_LCD_Printf(1,2, "End Der");
         }
-        if (Y < 0){
-            //Enderezar izquierda
-            LS_MT_TurnLeft(LS_MT_GetTimeFromDistance(5, 100), 100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "E Izq");
-            //Velocitats diferents (50 -> 10) en els dos enderezar
+        if (Y < 5){
+            //Cerca esquerra
+            GD(10, 100, 0);
+            LS_LCD_Printf(1,2, "End Izq");
         }
+        AcGetXYZ(&Xf, &Yf, &Zf); X = Xf; Y = Yf; Z = Zf;
     }
     
-    if(enderezado == 0) enderezado = 1;
-    
-    //Diferentes situaciones de subir de frente
-    if(X < 10){
-        
-        //Subir recto
-        if(Y > -5 && Y < 5){
-            LS_MT_Lineal(LS_MT_GetTimeFromDistance(15, 100), 100, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Subir");
-        }
-        
-        //Subir girando a derecha
-        if(Y < -10){
-            LS_MT_TurnRight(LS_MT_GetTimeFromDistance(3, 100), 100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Subir Der");
-        }
-        
-        //Subir girando a izquierda
-        if(Y > 10){
-            LS_MT_TurnLeft(LS_MT_GetTimeFromDistance(3, 100), 100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Subir Izq");
-        }
-    }
-    
-    //Diferentes situaciones de subir marcha atras
-    /*if(X > 10){
-        
-        //Subir recto
-        if(Y > -5 && Y < 5){
-            LS_MT_Lineal(LS_MT_GetTimeFromDistance(15, -100), -100, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Bajar");
-        }
-        
-        //Subir girando a derecha
-        if(Y < -10){
-            LS_MT_TurnLeft(LS_MT_GetTimeFromDistance(3, -100), -100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Bajar Der");
-        }
-        
-        //Subir girando a izquierda
-        if(Y > 10){
-            LS_MT_TurnRight(LS_MT_GetTimeFromDistance(3, -100), -100, 0, 0, &stopReason);
-            LS_LCD_Printf(1,2, "Bajar Izq");
-        }
-    }*/
-	
-    return enderezado;
+    // Avanç de pujada
+    upYouGo(X, Y, Z);
+	return;
 }
 
-void Inicialitza(void) {
-	LS_Init();	// Inicialitza la API de l'LSMaker
-	// Aqu? vindrien les inicialitzacions pr?pies de l'aplicaci?
-}
 
+/**
+ * Entrada al encendre.
+ * @return Exit code.
+ */
 int main(void) { 
-	// Aqu? comen?a el programa.
     
-    int start = 0, enderezado = 0;
+    //Flag que s'activa quan es pulsa GPButton i que dona inici a la
+    //pujada en costa
+    int start = 0;
+	
+    //Carga de la LSAPI
+    LS_Init();
     
-	Inicialitza();
-	LS_SYS_AddStateMachine(MotorInfo, 250);	// Una m?quina d'estats per refrescar l'LCD cada 250ms
-	while (1) {	// El bucle principal
+    // Mostrara constantment les components de l'acceleroetre per l'LCD
+	LS_SYS_AddStateMachine(MotorInfo, 250);
+    
+	while (1) {
         
-        if (LS_IO_GpButtonPress()) start = 1;
+        if (LS_IO_GpButtonPress()) 
+            start = 1;
         
         if(start)
-            enderezado = ProvaDeCalibracio(enderezado); 
+            Main(); 
  	}
 	return 0;
 }
